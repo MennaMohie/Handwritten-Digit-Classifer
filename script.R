b4304a730654aecbf5a27f74608717aeb1f50da5
mennamohie <mennaxmohiex@gmail.com>
2018-04-17 21:08:17 +0200

    Fixed. Final accuracy = 82.08%

--- script.R
+++ script.R
@@ -6,7 +6,7 @@ tests <- list(data[[2]],data[[4]],data[[6]],data[[8]],
 patterns <- sapply(data,function(x) colMeans(x));
 patterns <- matrix(
   c(patterns[,1],patterns[,3],patterns[,5],patterns[,7],patterns[,9],
-    patterns[,11],patterns[,11],patterns[,13],patterns[,15],patterns[,17]),
+    patterns[,11],patterns[,13],patterns[,15],patterns[,17], patterns[,19]),
   nrow=10, ncol = 784, byrow=TRUE
 );
 
@@ -31,5 +31,4 @@ for(i in 1:10)
 correct <- 0;
 for(i in 1:10)
   correct<- correct + length(which(results[[i]]==(i-1)));
-accuracy <- (correct/10000)*100;
-accuracy
+accuracy <- (correct/10000)*100
