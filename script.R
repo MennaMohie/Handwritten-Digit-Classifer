fe8a7f76a1993642ea9c70f60386c251e385a21a
mennamohie <mennaxmohiex@gmail.com>
2018-04-20 16:39:10 +0200

    Final touches. 7aswaka.

--- script.R
+++ script.R
@@ -2,22 +2,18 @@
 
 data <- R.matlab::readMat("mnist_all.mat");
 
-tests <- list(data[[2]],data[[4]],data[[6]],data[[8]],
-              data[[10]],data[[12]],data[[14]],data[[16]],data[[18]],data[[20]]);
+trains <- list(data[[1]], data[[3]], data[[5]], data[[7]], data[[9]], data[[11]], data[[13]], data[[15]], data[[17]], data[[19]]);
+tests  <- list(data[[2]], data[[4]], data[[6]], data[[8]], data[[10]], data[[12]], data[[14]], data[[16]], data[[18]], data[[20]]);
 
-patterns <- sapply(data,function(x) colMeans(x));
-patterns <- matrix(
-  c(patterns[,1],patterns[,3],patterns[,5],patterns[,7],patterns[,9],
-    patterns[,11],patterns[,13],patterns[,15],patterns[,17], patterns[,19]),
-  nrow=10, ncol = 784, byrow=TRUE
-);
+patterns <- sapply(trains,function(x) colMeans(x));
+patterns <- matrix(c(patterns), nrow=10, ncol = 784, byrow=TRUE);
 
 correlations = lapply(1:10, function(x) matrix(nrow=nrow(tests[[x]]),ncol=10));
 for(i in 1:10)
 {
   for(j in 1:10)
   {
-    correlations[[i]][,j] = apply(tests[[i]],1,function(x) cor(x,patterns[j,]))
+    correlations[[i]][,j] = apply(tests[[i]], 1, function(x) cor(x,patterns[j,]))
   }
 };
 
